e46622d3be98ae59a400bfe9d8a07345079db581
Eric Cornelissen <ericornelissen@gmail.com>
2025-04-12 21:01:03 +0200

    Add `permissions:` at the workflow level

--- workflow.go
+++ workflow.go
@@ -12,6 +12,7 @@ import (
 type Workflow struct {
 	Name        string            `yaml:"name,omitempty"`
 	RunName     string            `yaml:"run-name,omitempty"`
+	Permissions Permissions       `yaml:"permissions,omitempty"`
 	Concurrency Concurrency       `yaml:"concurrency,omitempty"`
 	Defaults    Defaults          `yaml:"defaults,omitempty"`
 	Env         map[string]string `yaml:"env,omitempty"`
@@ -38,6 +39,180 @@ type DefaultsRun struct {
 type Job struct {
 	Name  string `yaml:"name,omitempty"`
 	Steps []Step `yaml:"steps"`
+}
+
+// Permissions is a model of a GitHub Actions `permissions:` object.
+type Permissions struct {
+	Actions        string `yaml:"actions,omitempty"`
+	Attestations   string `yaml:"attestations,omitempty"`
+	Checks         string `yaml:"checks,omitempty"`
+	Contents       string `yaml:"contents,omitempty"`
+	Deployments    string `yaml:"deployments,omitempty"`
+	Discussions    string `yaml:"discussions,omitempty"`
+	IdToken        string `yaml:"id-token,omitempty"`
+	Issues         string `yaml:"issues,omitempty"`
+	Models         string `yaml:"models,omitempty"`
+	Packages       string `yaml:"packages,omitempty"`
+	Pages          string `yaml:"pages,omitempty"`
+	PullRequests   string `yaml:"pull-requests,omitempty"`
+	SecurityEvents string `yaml:"security-events,omitempty"`
+	Statuses       string `yaml:"statuses,omitempty"`
+}
+
+func (p *Permissions) UnmarshalYAML(n *yaml.Node) error {
+	all := func(s string) {
+		p.Actions = s
+		p.Attestations = s
+		p.Checks = s
+		p.Contents = s
+		p.Deployments = s
+		p.Discussions = s
+		p.IdToken = s
+		p.Issues = s
+		p.Models = s
+		p.Packages = s
+		p.Pages = s
+		p.PullRequests = s
+		p.SecurityEvents = s
+		p.Statuses = s
+	}
+
+	switch n.Kind {
+	case yaml.ScalarNode:
+		switch n.Value {
+		case "read-all":
+			all("read")
+		case "write-all":
+			all("write")
+		default:
+			return fmt.Errorf("invalid permissions value %s", n.Value)
+		}
+	case yaml.MappingNode:
+		var perms map[string]string
+		_ = n.Decode(&perms)
+
+		all("none")
+		if v, ok := perms["actions"]; ok {
+			p.Actions = v
+		}
+		if v, ok := perms["attestations"]; ok {
+			p.Attestations = v
+		}
+		if v, ok := perms["checks"]; ok {
+			p.Checks = v
+		}
+		if v, ok := perms["contents"]; ok {
+			p.Contents = v
+		}
+		if v, ok := perms["deployments"]; ok {
+			p.Deployments = v
+		}
+		if v, ok := perms["discussions"]; ok {
+			p.Discussions = v
+		}
+		if v, ok := perms["id-token"]; ok {
+			p.IdToken = v
+		}
+		if v, ok := perms["issues"]; ok {
+			p.Issues = v
+		}
+		if v, ok := perms["models"]; ok {
+			p.Models = v
+		}
+		if v, ok := perms["issues"]; ok {
+			p.Issues = v
+		}
+		if v, ok := perms["models"]; ok {
+			p.Models = v
+		}
+		if v, ok := perms["issues"]; ok {
+			p.Issues = v
+		}
+		if v, ok := perms["packages"]; ok {
+			p.Packages = v
+		}
+		if v, ok := perms["pages"]; ok {
+			p.Pages = v
+		}
+		if v, ok := perms["pull-requests"]; ok {
+			p.PullRequests = v
+		}
+		if v, ok := perms["security-events"]; ok {
+			p.SecurityEvents = v
+		}
+		if v, ok := perms["statuses"]; ok {
+			p.Statuses = v
+		}
+	default:
+		return fmt.Errorf("invalid permissions %s", n.Value)
+	}
+
+	return nil
+}
+
+func (p Permissions) MarshalYAML() (interface{}, error) {
+	all := func(s string) bool {
+		return p.Actions == s && p.Attestations == s && p.Checks == s && p.Contents == s && p.Deployments == s && p.Discussions == s && p.IdToken == s && p.Issues == s && p.Models == s && p.Packages == s && p.Pages == s && p.PullRequests == s && p.SecurityEvents == s && p.Statuses == s
+	}
+
+	n := yaml.Node{}
+	switch {
+	case all("read"):
+		n.Kind = yaml.ScalarNode
+		n.Tag = "!!str"
+		n.Value = "read-all"
+	case all("write"):
+		n.Kind = yaml.ScalarNode
+		n.Tag = "!!str"
+		n.Value = "write-all"
+	default:
+		perms := make(map[string]string, 14)
+		if v := p.Actions; v != "none" {
+			perms["actions"] = v
+		}
+		if v := p.Attestations; v != "none" {
+			perms["attestations"] = v
+		}
+		if v := p.Checks; v != "none" {
+			perms["checks"] = v
+		}
+		if v := p.Contents; v != "none" {
+			perms["contents"] = v
+		}
+		if v := p.Deployments; v != "none" {
+			perms["deployments"] = v
+		}
+		if v := p.Discussions; v != "none" {
+			perms["discussions"] = v
+		}
+		if v := p.IdToken; v != "none" {
+			perms["id-token"] = v
+		}
+		if v := p.Issues; v != "none" {
+			perms["issues"] = v
+		}
+		if v := p.Models; v != "none" {
+			perms["models"] = v
+		}
+		if v := p.Packages; v != "none" {
+			perms["packages"] = v
+		}
+		if v := p.Pages; v != "none" {
+			perms["pages"] = v
+		}
+		if v := p.PullRequests; v != "none" {
+			perms["pull-requests"] = v
+		}
+		if v := p.SecurityEvents; v != "none" {
+			perms["security-events"] = v
+		}
+		if v := p.Statuses; v != "none" {
+			perms["statuses"] = v
+		}
+		_ = n.Encode(perms)
+	}
+
+	return n, nil
 }
 
 // ParseWorkflow parses a GitHub Actions workflow into a [Workflow].
--- workflow_test.go
+++ workflow_test.go
@@ -136,7 +136,6 @@ jobs:
 			model: Workflow{
 				Jobs: map[string]Job{
 					"example": {
-						Name: "",
 						Steps: []Step{
 							{
 								Uses: Uses{
@@ -165,13 +164,172 @@ jobs:
 			model: Workflow{
 				Jobs: map[string]Job{
 					"example": {
-						Name: "",
 						Steps: []Step{
 							{
 								Uses: Uses{
 									Name:       "actions/checkout",
 									Ref:        "0ad4b8fadaa221de15dcec353f45205ec38ea70b",
 									Annotation: "v4",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"some permissions": {
+			yaml: `
+permissions:
+    actions: read
+    checks: write
+    packages: write
+    statuses: read
+jobs:
+    example:
+        steps:
+            - uses: actions/checkout@v4
+`,
+			model: Workflow{
+				Permissions: Permissions{
+					Actions:        "read",
+					Attestations:   "none",
+					Checks:         "write",
+					Contents:       "none",
+					Deployments:    "none",
+					Discussions:    "none",
+					IdToken:        "none",
+					Issues:         "none",
+					Models:         "none",
+					Packages:       "write",
+					Pages:          "none",
+					PullRequests:   "none",
+					SecurityEvents: "none",
+					Statuses:       "read",
+				},
+				Jobs: map[string]Job{
+					"example": {
+						Steps: []Step{
+							{
+								Uses: Uses{
+									Name: "actions/checkout",
+									Ref:  "v4",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"permissions: read-all": {
+			yaml: `
+permissions: read-all
+jobs:
+    example:
+        steps:
+            - uses: actions/checkout@v4
+`,
+			model: Workflow{
+				Permissions: Permissions{
+					Actions:        "read",
+					Attestations:   "read",
+					Checks:         "read",
+					Contents:       "read",
+					Deployments:    "read",
+					Discussions:    "read",
+					IdToken:        "read",
+					Issues:         "read",
+					Models:         "read",
+					Packages:       "read",
+					Pages:          "read",
+					PullRequests:   "read",
+					SecurityEvents: "read",
+					Statuses:       "read",
+				},
+				Jobs: map[string]Job{
+					"example": {
+						Steps: []Step{
+							{
+								Uses: Uses{
+									Name: "actions/checkout",
+									Ref:  "v4",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"permissions: write-all": {
+			yaml: `
+permissions: write-all
+jobs:
+    example:
+        steps:
+            - uses: actions/checkout@v4
+`,
+			model: Workflow{
+				Permissions: Permissions{
+					Actions:        "write",
+					Attestations:   "write",
+					Checks:         "write",
+					Contents:       "write",
+					Deployments:    "write",
+					Discussions:    "write",
+					IdToken:        "write",
+					Issues:         "write",
+					Models:         "write",
+					Packages:       "write",
+					Pages:          "write",
+					PullRequests:   "write",
+					SecurityEvents: "write",
+					Statuses:       "write",
+				},
+				Jobs: map[string]Job{
+					"example": {
+						Steps: []Step{
+							{
+								Uses: Uses{
+									Name: "actions/checkout",
+									Ref:  "v4",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		"permissions: {}": {
+			yaml: `
+permissions: {}
+jobs:
+    example:
+        steps:
+            - uses: actions/checkout@v4
+`,
+			model: Workflow{
+				Permissions: Permissions{
+					Actions:        "none",
+					Attestations:   "none",
+					Checks:         "none",
+					Contents:       "none",
+					Deployments:    "none",
+					Discussions:    "none",
+					IdToken:        "none",
+					Issues:         "none",
+					Models:         "none",
+					Packages:       "none",
+					Pages:          "none",
+					PullRequests:   "none",
+					SecurityEvents: "none",
+					Statuses:       "none",
+				},
+				Jobs: map[string]Job{
+					"example": {
+						Steps: []Step{
+							{
+								Uses: Uses{
+									Name: "actions/checkout",
+									Ref:  "v4",
 								},
 							},
 						},
@@ -205,6 +363,18 @@ jobs:
 	}
 
 	errCases := map[string]TestCase{
+		"yaml: invalid 'permissions' value, scalar": {
+			yaml: `
+permissions: 3.14
+`,
+		},
+		"yaml: invalid 'permissions' value, non-scalar": {
+			yaml: `
+permissions:
+- foo
+- bar
+`,
+		},
 		"yaml: invalid 'jobs' value": {
 			yaml: `
 jobs: 3.14
@@ -308,16 +478,6 @@ func checkWorkflow(t *testing.T, got, want *Workflow) {
 		t.Errorf("Unexpected name (got %q, want %q)", got, want)
 	}
 
-	if got, want := got.Jobs, want.Jobs; len(got) != len(want) {
-		t.Errorf("Unexpected number of jobs (got %d, want %d)", len(got), len(want))
-	} else {
-		for name := range want {
-			if _, ok := got[name]; !ok {
-				t.Errorf("Want job named %q but it is not present", name)
-			}
-		}
-	}
-
 	if got, want := got.RunName, want.RunName; got != want {
 		t.Errorf("Unexpected run-name (got %q, want %q)", got, want)
 	}
@@ -338,6 +498,7 @@ func checkWorkflow(t *testing.T, got, want *Workflow) {
 		t.Errorf("Unexpected defaults.run.working-directory (got %q, want %q)", got, want)
 	}
 
+	checkPermissions(t, got.Permissions, want.Permissions)
 	checkMap(t, got.Env, want.Env)
 	checkJobs(t, got.Jobs, want.Jobs)
 }
@@ -373,4 +534,66 @@ func checkJob(t *testing.T, got, want *Job) {
 	}
 
 	checkSteps(t, got.Steps, want.Steps)
+}
+
+func checkPermissions(t *testing.T, got, want Permissions) {
+	if got, want := got.Actions, want.Actions; got != want {
+		t.Errorf("Unexpected permission for 'actions' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Attestations, want.Attestations; got != want {
+		t.Errorf("Unexpected permission for 'attestations' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Checks, want.Checks; got != want {
+		t.Errorf("Unexpected permission for 'checks' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Contents, want.Contents; got != want {
+		t.Errorf("Unexpected permission for 'contents' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Deployments, want.Deployments; got != want {
+		t.Errorf("Unexpected permission for 'deployments' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Discussions, want.Discussions; got != want {
+		t.Errorf("Unexpected permission for 'discussions' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.IdToken, want.IdToken; got != want {
+		t.Errorf("Unexpected permission for 'id-token' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Issues, want.Issues; got != want {
+		t.Errorf("Unexpected permission for 'issues' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Models, want.Models; got != want {
+		t.Errorf("Unexpected permission for 'models' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Issues, want.Issues; got != want {
+		t.Errorf("Unexpected permission for 'issues' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Packages, want.Packages; got != want {
+		t.Errorf("Unexpected permission for 'packages' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Pages, want.Pages; got != want {
+		t.Errorf("Unexpected permission for 'pages' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.PullRequests, want.PullRequests; got != want {
+		t.Errorf("Unexpected permission for 'pull-requests' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.SecurityEvents, want.SecurityEvents; got != want {
+		t.Errorf("Unexpected permission for 'security-events' (got %q, want %q)", got, want)
+	}
+
+	if got, want := got.Statuses, want.Statuses; got != want {
+		t.Errorf("Unexpected permission for 'statuses' (got %q, want %q)", got, want)
+	}
 }
